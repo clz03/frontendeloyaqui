997c7e5d953d3da5e5dc8c9c9593119e6179baff
clz03 <cleber.znch@gmail.com>
2020-02-19 09:34:46 -0300

    alterando para build

---
+++ build/precache-manifest.fbfee6b3140e615d77a28c1da5c3bdd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad91085cc0ef753131aeb8c5ddd0534b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1aa875988ed11e1bae9",
+    "url": "/static/css/main.1eb3c9b4.chunk.css"
+  },
+  {
+    "revision": "149ed316dedc5ea01826",
+    "url": "/static/js/2.7d3ab4d5.chunk.js"
+  },
+  {
+    "revision": "e1aa875988ed11e1bae9",
+    "url": "/static/js/main.588d1422.chunk.js"
+  },
+  {
+    "revision": "61893c448ea7db2346c1",
+    "url": "/static/js/runtime-main.8eaca5f1.js"
+  },
+  {
+    "revision": "a2d08750e31391d8417a2396fdb8e55f",
+    "url": "/static/media/app-store.a2d08750.png"
+  },
+  {
+    "revision": "445ecd8407bb54a2477891f4f9f6e3f1",
+    "url": "/static/media/google-play.445ecd84.png"
+  },
+  {
+    "revision": "b8613f3c2301a9a6057898ba224e1604",
+    "url": "/static/media/loading.b8613f3c.gif"
+  }
+]);
